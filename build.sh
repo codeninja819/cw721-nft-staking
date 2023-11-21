b6131bea89414d0d67395ba3411f5ca2ec20e305
codeninja819 <jiromatsumoto98@gmail.com>
2023-11-21 11:47:17 -0500

    update build.sh

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@
 docker run --rm -v "$(pwd)":/code \
   --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
   --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
-  cosmwasm/workspace-optimizer:0.12.12
+  cosmwasm/rust-optimizer:0.12.12
