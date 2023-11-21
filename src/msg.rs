a1e1bd62ff5ef2fa76b87a02d7c31157f5f5f39c
codeninja819 <jiromatsumoto98@gmail.com>
2023-11-21 15:40:33 -0500

    add bin/schema.rs

--- src/msg.rs
+++ src/msg.rs
@@ -1,13 +1,11 @@
+use cosmwasm_schema::{cw_serde, QueryResponses};
 use cosmwasm_std::{Coin, Timestamp};
 use cw721::Cw721ReceiveMsg;
-use schemars::JsonSchema;
-use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[cw_serde]
 pub struct InstantiateMsg {}
 
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-#[serde(rename_all = "snake_case")]
+#[cw_serde]
 pub enum ExecuteMsg {
     TransferOwnership {
         address: String,
@@ -27,26 +25,36 @@ pub enum ExecuteMsg {
     },
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-#[serde(rename_all = "snake_case")]
+#[cw_serde]
+#[derive(QueryResponses)]
 pub enum QueryMsg {
+    #[returns(ConfigResponse)]
     GetConfig {},
+    #[returns(Vec<CollectionResponse>)]
     GetCollections {},
+    #[returns(Vec<StakingResponse>)]
     GetStakingsByOwner { owner: String },
 }
 
 // responses
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-#[serde(rename_all = "snake_case")]
+#[cw_serde]
 pub struct ConfigResponse {
     pub owner: String,
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-#[serde(rename_all = "snake_case")]
+#[cw_serde]
 pub struct CollectionResponse {
     pub address: String,
     pub reward: Coin,
     pub cycle: u64,
     pub is_whitelisted: bool,
+}
+
+#[cw_serde]
+pub struct StakingResponse {
+    pub token_address: String, // nft collection ca
+    pub token_id: String,
+    pub start_timestamp: Timestamp,
+    pub end_timestamp: Timestamp,
+    pub is_paid: bool,
 }
